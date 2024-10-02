2785ed264f981064911bc90a0059ce9b8db7330f
abotstheboss <abolarinoluwatobistephen@gmail.com>
2024-10-02 10:31:53 -0500

    feat: MapCell

--- src/map.rs
+++ src/map.rs
@@ -1,12 +1,17 @@
 use std::{
+    cell::RefCell,
     fs,
     io::{self, BufRead, ErrorKind},
+    rc::Rc,
 };
 
-use crate::config::Config;
+use crate::{
+    config::Config,
+    map_cell::{CellType, MapCell},
+};
 
 pub struct Map {
-    grid: Vec<Vec<char>>,
+    grid: Vec<Vec<Rc<RefCell<MapCell>>>>,
 }
 
 impl Map {
@@ -31,25 +36,86 @@ impl Map {
         let reader = io::BufReader::new(file);
         let mut grid = vec![];
 
-        for line in reader.lines() {
+        for (y, line) in reader.lines().enumerate() {
             let line = line.expect("Error. Unexpected error while reading line");
             let split = line.split(",");
             let mut row = vec![];
-
-            for region in split {
-                row.push(region.chars().next().unwrap());
+            for (x, region) in split.enumerate() {
+                row.push(Rc::new(RefCell::new(MapCell::new(
+                    x,
+                    y,
+                    region.chars().next().unwrap(),
+                ))));
             }
 
             grid.push(row);
         }
 
-        return Self { grid };
+        let new_map = Self { grid };
+        new_map.trackAdjacentCells();
+
+        return new_map;
+    }
+
+    fn trackAdjacentCells(&self) {
+        for row in &self.grid {
+            for cell in row {
+                let (x, y) = cell.borrow().position;
+                // let mut adjacentCells = vec![];
+                if let CellType::Other(_) = cell.borrow().type_ {
+                    continue;
+                }
+
+                if x > 0 {
+                    cell.borrow_mut()
+                        .adjacentCells
+                        .push(Rc::clone(&self.grid[y][x - 1]));
+
+                    if y > 0 {
+                        cell.borrow_mut()
+                            .adjacentCells
+                            .push(Rc::clone(&self.grid[y - 1][x - 1]));
+                    }
+                    if y < self.grid.len() - 1 {
+                        cell.borrow_mut()
+                            .adjacentCells
+                            .push(Rc::clone(&self.grid[y + 1][x - 1]));
+                    }
+                }
+                if x < self.grid[y].len() - 1 {
+                    cell.borrow_mut()
+                        .adjacentCells
+                        .push(Rc::clone(&self.grid[y][x + 1]));
+
+                    if y > 0 {
+                        cell.borrow_mut()
+                            .adjacentCells
+                            .push(Rc::clone(&self.grid[y - 1][x + 1]));
+                    }
+                    if y < self.grid.len() - 1 {
+                        cell.borrow_mut()
+                            .adjacentCells
+                            .push(Rc::clone(&self.grid[y + 1][x + 1]));
+                    }
+                }
+                if y > 0 {
+                    cell.borrow_mut()
+                        .adjacentCells
+                        .push(Rc::clone(&self.grid[y - 1][x]));
+                }
+                if y < self.grid.len() - 1 {
+                    cell.borrow_mut()
+                        .adjacentCells
+                        .push(Rc::clone(&self.grid[y + 1][x]));
+                }
+            }
+        }
     }
 
     pub fn print(&self) {
         for row in &self.grid {
             for region in row {
-                print!("{}", region);
+                print!("{}", region.borrow());
             }
             println!();
         }
--- src/map_cell.rs
+++ src/map_cell.rs
@@ -1,13 +1,52 @@
-pub struct CellData {
-    posiiton: (u32, u32),
-    symbol: char,
+use std::{cell::RefCell, fmt, rc::Rc};
+
+pub struct MapCell {
+    pub position: (usize, usize),
+    pub type_: CellType,
     population: u32,
     pollution: u32,
+    isPowerlineAdjacent: bool,
+    pub adjacentCells: Vec<Rc<RefCell<MapCell>>>,
 }
 
-pub enum MapCell {
-    Residential(CellData),
-    Industrial(CellData),
-    Commercial(CellData),
-    Other(CellData),
+pub enum CellType {
+    Residential(char),
+    Industrial(char),
+    Commercial(char),
+    Other(char),
+}
+
+impl MapCell {
+    pub fn new(x: usize, y: usize, symbol: char) -> Self {
+        Self {
+            position: (x, y),
+            type_: match symbol {
+                'R' => CellType::Residential(symbol),
+                'I' => CellType::Industrial(symbol),
+                'C' => CellType::Commercial(symbol),
+                _ => CellType::Other(symbol),
+            },
+            population: 0,
+            pollution: 0,
+            isPowerlineAdjacent: false,
+            adjacentCells: vec![],
+        }
+    }
+}
+
+impl fmt::Display for MapCell {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.type_)
+    }
+}
+
+impl fmt::Display for CellType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            CellType::Residential(symbol) => write!(f, "{}", symbol),
+            CellType::Industrial(symbol) => write!(f, "{}", symbol),
+            CellType::Commercial(symbol) => write!(f, "{}", symbol),
+            CellType::Other(symbol) => write!(f, "{}", symbol),
+        }
+    }
 }
